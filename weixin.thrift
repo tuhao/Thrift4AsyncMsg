21cabef1fd22fd072f4c4f3a32a507ca086c01be
Ikfly <astro.liuhang@gmail.com>
2014-01-04 06:23:03 +0800

    wenxin.thrift

---
+++ weixin.thrift
@@ -0,0 +1,21 @@
+struct Article {
+	1: string title,
+	2: string description,
+	3: string imageurl,
+	4: string url,
+}
+
+struct News {
+	1: string title,
+	2: list<Article> articles,
+}
+
+struct Message {
+	1: string title,
+	2: string content,	
+}
+
+service DataService {
+	bool pushMsg(1: list<Message> data),
+	bool pushNews(1: list<News> data),
+}
