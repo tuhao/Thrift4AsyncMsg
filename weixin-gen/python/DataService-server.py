191deebb382d139ac7cfea363b345feb06654faf
unknown <astro.liuhang@gmail.com>
2014-01-21 18:01:28 +0800

    fix

--- weixin-gen/python/DataService-server.py
+++ weixin-gen/python/DataService-server.py
@@ -5,75 +5,33 @@ from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 from ttypes import *
+from MySQLClient import WeixinDB
 import DataService
-import ConfigParser
-import MySQLdb
-import MySQLdb.cursors
 import time
 import datetime
 import sys
 reload(sys)
 sys.setdefaultencoding='utf-8'
 
-CFG_FILE = 'mysql.cfg'
-CFG_SESSION = 'connect'
-
-class WeixinDB:
-
-	def __init__(self,host,user,passwd,db_name,charset):
-		self.conn = MySQLdb.connect(host,user,passwd,db_name,charset=charset,cursorclass=MySQLdb.cursors.SSCursor)
-		self.cursor = self.conn.cursor()
-
-	def execute_insert(self,sql_str):
-		try:
-			self.cursor.execute(sql_str)
-		except Exception, e:
-			print e
-		return ()
-
-	def execute_query(self,sql_str):
-		try:
-			self.cursor.execute(sql_str)
-			return self.cursor.fetchall()
-		except Exception, e:
-			print e
-		return ()
-
-	def last_record(self):
-		return self.cursor.lastrowid
-
-	def __exit__(self,*kw):
-		self.conn.commit()
-
-	def __enter__(self):
-		pass
-
-	def rollback(self):
-		self.conn.rollback()
-
-	def close(self):
-		self.cursor.close()
-		self.conn.commit()
-		self.conn.close()
-
-
-class SyncDB(DataService.Iface):
+class ThriftHandler(DataService.Iface):
 
 	def pushMsg(self,data):
-		try:
-			start = time.localtime(time.time())
-			create_time = datetime.datetime(*start[:6])
-			with repo:
-				for msg in data:
-					sql = 'insert into signature_message (title,create_time,content) values ("%s","%s","%s")' % (msg.title,create_time,msg.content)
+		count = 0
+		create_time = time.strftime('%Y-%m-%d %H:%M:%S',datetime.datetime.now())
+		with repo:
+			for msg in data:
+				try:
+					sql = 'insert into signature_message (title,create_time,content,reason,sort_id) values ("%s","%s","%s","%s",%d)' % (msg.title,create_time,msg.content,None,1)
 					repo.execute_insert(sql)
-			print 'insert %d messages at %s' % (len(data),time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
-		except Exception, e:
-			print e
-			print ' at %s' % (time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
-			return False
-		else:
+					count = count + 1
+				except Exception, e:
+					print e
+					print ' at %s' % (create_time)
+		print 'insert %d messages at %s' % (count,create_time)
+		if count > 0:
 			return True
+		else:
+			return False
 
 	def pushNews(self,data):
 		try:
@@ -101,45 +59,61 @@ class SyncDB(DataService.Iface):
 		return True
 
 	def pullMsg(self,size):
+		count = 0
+		create_time = time.strftime('%Y-%m-%d %H:%M:%S',datetime.datetime.now())
 		result = list()
-		try:
-			with repo:
-				sql = """select * from signature_message order by id desc limit """ + str(size)
-				for item in repo.execute_query(sql):
-					if item[4] is None:
-						result.append(Message(title=item[1].encode('utf-8'),create_time=str(item[2]),content=item[3].encode('utf-8'),reason=str(item[4]).encode('utf-8')))
-					else:
-						result.append(Message(title=item[1].encode('utf-8'),create_time=str(item[2]),content=item[3].encode('utf-8'),reason=item[4].encode('utf-8')))
-			print 'pull %d messages at %s' % (size,time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
-		except MemoryError,er:
-			print er
-			return []
-		except Exception,e:
-			print e
-			print ' at %s' % (time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
-			return []
-		else:
-			return result
+		with repo:
+			sql = "select * from signature_message order by id desc limit %d " % (int(size))
+			try:
+				query_tuple = repo.execute_query(sql)
+			except Exception,e:
+				print e
+				print ' at %s' % (create_time)
+			else:
+				for item in query_tuple:
+					try:
+						reason = item[4]
+						if reason is None:
+							reason = 'None'
+						result.append(Message(title=item[1].encode('utf-8'),create_time=str(item[2]),content=item[3].encode('utf-8'),reason=reason.encode('utf-8')))
+						count = count + 1
+					except Exception, e:
+						print e
+						print ' at %s' % (create_time)
+					except MemoryError,er:
+							print er
+							print ' at %s' % (create_time)
+		print 'pull %d messages at %s' % (count,create_time)
+		return result
+
 
 	def pullMsgBySort(self,size,sort_id):
 		result = list()
-		try:
-			with repo:
-				sql = "select * from signature_message where sort_id = %d order by id desc limit %d " % (int(sort_id),int(size))
-				for item in repo.execute_query(sql):
-					if item[4] is None:
-						result.append(Message(id=int(item[0]),title=item[1].encode('utf-8'),create_time=str(item[2]),content=item[3].encode('utf-8'),reason=str(item[4]).encode('utf-8'),sort_id=int(item[5])))
-					else:
-						result.append(Message(id=int(item[0]),title=item[1].encode('utf-8'),create_time=str(item[2]),content=item[3].encode('utf-8'),reason=item[4].encode('utf-8'),sort_id=int(item[5])))
-		except MemoryError,er:
-			print er
-			return []
-		except Exception,e:
-			print e
-			print ' at %s' % (time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
-			return []
-		else:
-			return result
+		count = 0
+		create_time = time.strftime('%Y-%m-%d %H:%M:%S',datetime.datetime.now())
+		with repo:
+				try:
+					sql = "select * from signature_message where sort_id = %d order by id desc limit %d " % (int(sort_id),int(size))
+					query_tuple = repo.execute_query(sql)
+				except Exception,e:
+					print e
+					print ' at %s' % (create_time)
+				else:
+					for item in query_tuple(sql):
+						try:
+							reason = item[4]
+							if reason is None:
+								reason = 'None'
+							result.append(Message(id=int(item[0]),title=item[1].encode('utf-8'),create_time=str(item[2]),content=item[3].encode('utf-8'),reason=reason.encode('utf-8'),sort_id=int(item[5])))
+							count = count + 1
+						except Exception, e:
+							print e
+							print ' at %s' % (create_time)
+						except MemoryError,er:
+							print er
+							print ' at %s' % (create_time)
+		print 'pull %d messages at %s' % (count,create_time)
+		return result
 
 argi = 1
 server_address = '192.168.1.103'
@@ -156,16 +130,12 @@ if len(sys.argv) > 1:
     	if sys.argv[argi] == '-cfg':
     		CFG_FILE = sys.argv[argi+1]
 
-config = ConfigParser.ConfigParser()
-s = lambda name:config.get(CFG_SESSION,name)
-with open(CFG_FILE,'r') as cfg:
-	config.readfp(cfg)
-repo =  WeixinDB(s('host'),s('user'),s('passwd'),s('db_name'),s('charset'))
 
+repo =  WeixinDB()
 transport = TSocket.TServerSocket(server_address,port)
 transportFactory = TTransport.TFramedTransportFactory()
 protocolFactory = TBinaryProtocol.TBinaryProtocolFactory()
-handler = SyncDB()
+handler = ThriftHandler()
 processor = DataService.Processor(handler)
 server = TServer.TThreadPoolServer(processor,transport,transportFactory,protocolFactory)
 print "Starting thrift server in python..."
